b043cb062ebc6718060c7167fe96295f49245576
Raegan Millhollin <desdemonhu@gmail.com>
2018-02-12 13:10:18 -0800

    Added handlebars template so that maps api key stays in .env file, but it still gets injected into the html

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,32 @@
+'use strict';
+
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
+    resolve: {
+        alias: {
+            handlebars: 'handlebars/dist/handlebars.min.js'
+        }
+    },
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/dist'
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jQuery',
+            jQuery: 'jQuery'
+        }),
+        new Dotenv({
+            path: '.env',
+            safe: false
+        }),
+    ],
      module: {
         noParse: /node_modules\/knockout\/build\/output\/*.js/,
         rules: [
@@ -22,17 +40,10 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            { 
+                test: /\.handlebars$/, 
+                loader: "handlebars-loader" 
             }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jQuery',
-            jQuery: 'jQuery'
-        }),
-        new Dotenv({
-            path: '.env',
-            safe: false
-        })
     ]
-};
+}}

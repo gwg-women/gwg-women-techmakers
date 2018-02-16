6c4ac1ec2923187db34f25320dcc9fc501ade317
Amy Carney <click2carney@yahoo.com>
2018-02-16 13:24:56 -0900

    register service worker

---
+++ js/main/index.js
@@ -0,0 +1,16 @@
+import IndexController from './IndexController';
+
+const polyfillsNeeded = [];
+
+if (!('Promise' in self)) polyfillsNeeded.push('/js/polyfills/promise.js');
+
+try {
+  new URL('b', 'http://a');
+}
+catch (e) {
+  polyfillsNeeded.push('/js/polyfills/url.js');
+}
+
+loadScripts(polyfillsNeeded, function() {
+  new IndexController(document.querySelector('.main'));
+});
---
+++ js/sw/index.js
@@ -0,0 +1,18 @@
+var staticCacheName = 'gwgapp-static-v1';
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/skeleton'));
+      return;
+    }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});

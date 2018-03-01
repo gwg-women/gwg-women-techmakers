37cfa36417bcac69ae885720eb1ab21fa6747242
Sonali Shukla <sonalikatara@gmail.com>
2018-02-28 22:32:25 -0800

    feat: register service worker and add Progressive Enhancement

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+/* service worker */
+ if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+          navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+          }).catch(function(err) {
+            console.log(err)
+          });
+        });
+      } else {
+        console.log('service worker is not supported');
+      }
+/* end service worker **/
 
---
+++ src/service-worker.js
@@ -0,0 +1,56 @@
+
+// Set this to true for production
+const doCache = false;
+
+const CACHE_NAME = 'maapa-cache-v1';
+
+const urlsToCache = [
+  '/',
+  '/bundle.js',
+  '/index.js',
+  '/service-worker.js',
+  '/index.css',
+  '/app.js',
+  '/app.css',
+];
+
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys()
+      .then(keyList =>
+        Promise.all(keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            console.log('Deleting cache: ' + key)
+            return caches.delete(key);
+          }
+        }))
+      )
+  );
+});
+
+// The first time the user starts up the app, 'install' is triggered.
+self.addEventListener('install', function(event) {
+  if (doCache) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+              cache.addAll(urlsToCache)
+              console.log('cached');
+            })
+        })
+    );
+  }
+});
+
+// When the webpage goes to fetch files, we intercept that request and serve up the matching files
+// if we have them
+self.addEventListener('fetch', function(event) {
+    if (doCache) {
+      event.respondWith(
+          caches.match(event.request).then(function(response) {
+              return response || fetch(event.request);
+          })
+      );
+    }
+});

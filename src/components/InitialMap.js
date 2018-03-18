36a80fbacb30f60213994fd46ca5326246cf24d8
Renata Santoso <>
2018-03-18 09:39:35 -0700

    Fix the current position marker that was not center on the map.

--- src/components/InitialMap.js
+++ src/components/InitialMap.js
@@ -4,30 +4,28 @@ import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 class MapContainer extends Component {
   onGoogleMapLoad = map => {
     this.map = map;
-  }
+  }  
 
   render() {
     const containerStyle = {position: 'relative', width: '100%', height:'600px'}
-
-    const {pos} = this.props;
-
+    const {pos} = this.props;    
+    
     if(!this.props.loaded){
       return <div>loading...</div>
     }
-    return (
+
+    return (      
       <Map
         ref={this.onGoogleMapLoad}
         google={this.props.google}
         zoom={15}
         containerStyle={containerStyle}
-        center={pos}
-      >
+        initialCenter={pos}
+        center={pos}>
 
       <Marker
-        name={'Current Location'}
+        name={'Current Location'}        
         position={pos} />
-      <Marker />
-
       </Map>
     );
   }

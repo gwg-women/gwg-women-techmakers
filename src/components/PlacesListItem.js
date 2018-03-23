37648a099a9a5c027d2824664e7ef45b9071d27f
Sonali Shukla <sonalikatara@gmail.com>
2018-03-23 13:31:09 -0700

    fix: fix undefined error in placeListItems

--- src/components/PlacesListItem.js
+++ src/components/PlacesListItem.js
@@ -7,7 +7,7 @@ const PlacesListItem = (props) => {
     				<img className="listings-icon" src={props.place.icon} alt= ""/>  { props.place.name }
     				<div className="listings-info">
     				    {props.place.formatted_address.split(',')[0]}
-                    <div>{props.place.opening_hours.open_now?'OPEN NOW':'CLOSED'}</div>
+                    <div>{props.place.opening_hours === undefined ? '' : (props.place.opening_hours.open_now ? 'OPEN NOW':'CLOSED')}</div>
 
     				</div>
     			 </div>

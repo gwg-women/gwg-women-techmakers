bfd160e56e3333571f397faaeb570bebe6c9a743
Amy Carney <click2carney@yahoo.com>
2018-03-24 09:35:05 -0800

    install react-places-autocomplete

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,41 @@
 import React, {Component} from 'react';
-
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 
 class Search extends Component {
-
+    constructor(props) {
+        super(props)
+        this.state = { address: 'San Francisco, CA' }
+        this.onChange = (address) => this.setState({ address })
+    }
+    
+    handleFormSubmit = (event) => {
+        event.preventDefault()
+    
+        geocodeByAddress(this.state.address)
+          .then(results => getLatLng(results[0]))
+          .then(latLng => console.log('Success', latLng))
+          .catch(error => console.error('Error', error))
+    }
+    
     render() {
-        return (
-            <div className="search">
-                <input
-                    type="text"
-                    placeholder="I'm looking for...."
-                    className="search-box"
-                    onChange = {this.props.input}
-                />
-                <button onClick = {this.props.submit}>Submit</button>
-            </div>
+        const renderSuggestion = ({ suggestion }) => (<div><i className="fa fa-map-marker"/>{suggestion}</div>)
+
+        const inputProps = {
+            value: this.state.address,
+            onChange: this.onChange,
+            type: "search",
+            placeholder: "I'm looking for...",
+            className: "search-box",
+    }
+    
+    return (
+        <form onSubmit={this.handleFormSubmit} className="search">
+            <PlacesAutocomplete 
+                inputProps={inputProps}
+                renderSuggestion={renderSuggestion}
+             />
+            <button type="submit">Submit</button>
+          </form>
         )
     }
 }

859bf52fb9e57aeaaa9d186bf5bd47fb27a3fd7f
Pepper <jenearly@gmail.com>
2018-03-27 10:44:52 -0400

    made some changes to the css

--- src/components/PlacesListItem.js
+++ src/components/PlacesListItem.js
@@ -4,7 +4,7 @@ const PlacesListItem = (props) => {
 
     return <li >
                 <div className="listings">
-                    <div className="listings-icon">{props.place.photos === undefined ?<img src={props.place.icon} alt= ""/> : <img src={props.place.photos[0].getUrl({'maxWidth': 135, 'maxHeight': 135})} alt="" />}</div>
+                    <div className="listing-icon-div">{props.place.photos === undefined ?<img src={props.place.icon} alt= ""/> : <img className="listing-icon" src={props.place.photos[0].getUrl({'maxWidth': 135, 'maxHeight': 135})} alt="" />}</div>
                     <div className="listing-description">
                         <div className="listing-name">{ props.place.name }</div>
                         <div className="listings-info">

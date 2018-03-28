f2fd961812d4d7c0c9ce6b5bb1a48b198525a8b8
agonzalez0515 <agonzalez0515@gmail.com>
2018-03-27 22:25:28 -0700

    Add alt image tag to static map

--- src/components/StaticMap.js
+++ src/components/StaticMap.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 
-class StaticMap extends Component {
+class StaticMap extends Component { 
     
     render() {
         return (
             <div>
-                <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${this.props.pos.lat},${this.props.pos.lng}&zoom=14&size=600x600&key=${process.env.REACT_APP_STATICMAP}`} />
+                <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${this.props.pos.lat},${this.props.pos.lng}&zoom=14&size=600x600&key=${process.env.REACT_APP_STATICMAP}`} alt="Placeholder static map while network is down" />
             </div>
         )
     }

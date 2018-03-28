cf9d87d01a2f31e94ada945be6493b497fe51a5f
agonzalez0515 <agonzalez0515@gmail.com>
2018-03-27 21:21:10 -0700

    add cache code

--- public/sw.js
+++ public/sw.js
@@ -45,12 +45,49 @@ self.addEventListener('install', function(event) {
 
 // When the webpage goes to fetch files, we intercept that request and serve up the matching files
 // if we have them
+// self.addEventListener('fetch', function(event) {
+//     if (doCache) {
+//       event.respondWith(
+//           caches.match(event.request).then(function(response) {
+//               return response || fetch(event.request);
+//           })
+//       );
+//     }
+// });
+
+
+
+//this is caching the static map on the first load
+self.addEventListener('fetch', function (event) {
+
+  const requestUrl = new URL(event.request.url)
+  console.log(requestUrl)
+  if (requestUrl.pathname.startsWith('/maps/api/staticmap')) {
+    console.log("going to cache the static map")
+    // event.respondWith(
+    //   caches.open(CACHE_NAME).then(function(cache) {
+    //     return cache.match(event.request).then(function(response) {
+    //       return response || fetch(event.request).then(function(response) {
+    //         cache.put(event.request, response.clone())
+    //         return response
+    //       })
+    //     })
+    //   })
+    // )
+  }
+})
+
+
+//this is hijacking the request to the maps api, and if it fails, it serves the static image (happinesssssssss)
 self.addEventListener('fetch', function(event) {
-    if (doCache) {
-      event.respondWith(
-          caches.match(event.request).then(function(response) {
-              return response || fetch(event.request);
-          })
-      );
-    }
-});
+  const requestUrl = new URL(event.request.url)
+  
+  if (requestUrl.pathname.startsWith('/maps/api/js')) {
+    console.log('intercepting api')
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match('maps/api/staticmap')
+      })
+    )
+  }
+})

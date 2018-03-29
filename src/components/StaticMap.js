0083f0df65cded3b380c99b7309c5cb0167c86aa
Angelica Gonzalez <agonzalez0515@gmail.com>
2018-03-28 20:41:16 -0700

    Change API key variable so that we only have to use one for all Google products

--- src/components/StaticMap.js
+++ src/components/StaticMap.js
@@ -6,7 +6,7 @@ class StaticMap extends Component {
     render() {
         return (
             <div>
-                <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${this.props.pos.lat},${this.props.pos.lng}&zoom=14&size=600x600&key=${process.env.REACT_APP_STATICMAP}`} alt="Placeholder static map while network is down" />
+                <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${this.props.pos.lat},${this.props.pos.lng}&zoom=14&size=600x600&key=${process.env.REACT_APP_GKEY}`} alt="Placeholder static map while network is down" />
             </div>
         )
     }

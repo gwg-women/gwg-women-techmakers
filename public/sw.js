b56c2e1d08024b04d00ae2a9d3db73dc8ff9760a
Angelica Gonzalez <agonzalez0515@gmail.com>
2018-04-05 21:22:01 -0700

    remove extra fetch listener

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Set this to true for production
-const doCache = true;
+const doCache = false;
 
-const CACHE_NAME = 'maapa-cache-v7';
+const CACHE_NAME = 'maapa-cache-v12';
 
 const urlsToCache =[
   '/',
@@ -13,34 +13,21 @@ const urlsToCache =[
 
 // The first time the user starts up the app, 'install' is triggered.
 self.addEventListener('install', function(event) {
-  if (doCache) {
+  // if (doCache) {
     event.waitUntil(
       caches.open(CACHE_NAME)
         .then(function(cache) {
             return cache.addAll(urlsToCache)
         })
     );
-  }
+  // }
 });
 
 // When the webpage goes to fetch files, we intercept that request and serve up the matching files
 // if we have them
 self.addEventListener('fetch', function(event) {
-  if (doCache) {
-      event.respondWith(
-          caches.match(event.request).then(function(response) {
-              return response || fetch(event.request);
-          })
-      );
-  }
-});
 
-
-
-//This is caching the static map on the first load, Called "Cache then network" recipe
-self.addEventListener('fetch', function (event) {
   const requestUrl = new URL(event.request.url)
-  
   if (requestUrl.pathname.startsWith('/maps/api/staticmap')) {
     console.log("caching static map")
     event.respondWith(
@@ -53,8 +40,36 @@ self.addEventListener('fetch', function (event) {
         })
       })
     )
+  } else {
+  
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
   }
-})
+});
+
+
+
+//This is caching the static map on the first load, Called "Cache then network" recipe
+// self.addEventListener('fetch', function (event) {
+//   const requestUrl = new URL(event.request.url)
+  
+//   if (requestUrl.pathname.startsWith('/maps/api/staticmap')) {
+//     console.log("caching static map")
+//     event.respondWith(
+//       caches.open(CACHE_NAME).then(function(cache) {
+//         return cache.match(event.request).then(function(response) {
+//           return response || fetch(event.request).then(function(response) {
+//             cache.put(event.request, response.clone())
+//             return response
+//           })
+//         })
+//       })
+//     )
+//   }
+// })
 
 
 //this is hijacking the request to the maps api, and if it fails, it serves the static image (happinesssssssss)
@@ -72,7 +87,9 @@ self.addEventListener('fetch', function(event) {
 
 
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", function(event) {
+  self.clients.claim();
+
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys()

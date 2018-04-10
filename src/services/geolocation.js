f1906c9b0f738706670158a9a2962baf6b16c839
Pepper <jenearly@gmail.com>
2018-04-10 12:48:45 -0400

    removed Washington (dc) because it was redundant with the result

--- src/services/geolocation.js
+++ src/services/geolocation.js
@@ -1,5 +1,80 @@
 export function getCity(lat, lng){
   let city;
+  const state = {
+    //Canada
+        "AB": "Alberta",
+        "BC": "British Columbia",
+        "MB": "Manitoba",
+        "NB": "New Brunswick",
+        "NF": "Newfoundland",
+        "NS": "Nova Scotia",
+        "NT": "Northwest Territories",
+        "ON": "Ontario",
+        "PE": "Prince Edward Island",
+        "QC": "Quebec",
+        "SK": "Saskatchewan",
+        "YT": "Yukon",
+    //USA
+        "AK": "Alaska",
+        "AL": "Alabama",
+        "AR": "Arkansas",
+        "AS": "American Samoa",
+        "AZ": "Arizona",
+        "CA": "California",
+        "CO": "Colorado",
+        "CT": "Connecticut",
+        "DC": "District of Columbia",
+        "DE": "Deleware",
+        "FL": "Florida",
+        "FM": "Federated State of Micronesia",
+        "GA": "Georgia",
+        "GU": "Guam",
+        "HI": "Hawaii",
+        "IA": "Iowa",
+        "ID": "Idaho",
+        "IL": "Illinois",
+        "IN": "Indiana",
+        "KS": "Kansas",
+        "KY": "Kentucky",
+        "LA": "Lousiana",
+        "MA": "Massachusetts",
+        "MD": "Maryland",
+        "ME": "Maine",
+        "MH": "Marshall Islands",
+        "MI": "Michigan",
+        "MN": "Minnesota",
+        "MO": "Missouri",
+        "MP": "Northern Mariana Islands",
+        "MS": "Mississippi",
+        "MT": "Montana",
+        "NC": "North Carolina",
+        "ND": "North Dakota",
+        "NE": "Nebraska",
+        "NH": "New Hampshire",
+        "NJ": "New Jersey",
+        "NM": "New Mexico",
+        "NV": "Nevada",
+        "NY": "New York",
+        "OH": "Ohio",
+        "OK": "Oklahoma",
+        "OR": "Oregon",
+        "PA": "Pennsylvania",
+        "PR": "Puerto Rico",
+        "PW": "Palau",
+        "RI": "Rhode Island",
+        "SC": "South Carolina",
+        "SD": "South Dakota",
+        "TN": "Tennessee",
+        "TX": "Texas",
+        "UT": "Utah",
+        "VA": "Virginia",
+        "VI": "Virgin Island",
+        "VT": "Vermont",
+        "WA": "Washington",
+        "WI": "Wisconsin",
+        "WV": "West Virginia",
+        "WY": "Wyoming"
+          }
 
 
   return new Promise(function (resolve, reject) {
@@ -17,18 +92,17 @@ export function getCity(lat, lng){
         if (results[0]) {
           
           city = results[0].formatted_address;
-          console.log('unmodified address: ' + city)
+          console.log('this is the original location ' + city)
+          // Replaces state abbreviation to full name using state object.
+          Object.keys(state).forEach(k => city = city.replace(new RegExp(`\\b${k}\\b`, 'g'), state[k]));
 
           //gets rid of 'unnamed road', 'USA', numbers with their word if connected, and -=
           city = city.replace(/\b\w*?Unnamed Road|-|USA|[a-z]*\d+[a-z]*\w*?\b/gi, '');
-          
-
-            //split by ','
-            let citystate = city.split(',');
+          //split by ','
+          let citystate = city.split(',');
             
-            //gets rid of those empty array elements as well as remove empty space in front and back of array elements.
-            citystate = citystate.filter(entry => entry.trim() !== '').map(string => string.trim());
-            console.log('fully modified address: ' + 'length: ' + citystate.length + ' location: ' + citystate)
+          //gets rid of those empty array elements as well as remove empty space in front and back of array elements.
+          citystate = citystate.filter(entry => entry.trim() !== '').map(string => string.trim());
 
               if (citystate.length >= 4) {
                 citystate = `${[citystate[citystate.length -3]]}, ${[citystate[citystate.length -2]]}, ${[citystate[citystate.length -1]]}`
